aeed9ef4776828f6f415378133f6aa96bd1c19c8
kndrckm <kendrick.marzuki@gmail.com>
2026-03-06 02:38:08 +0700

    Update project icons and favicon

--- vite.config.ts
+++ vite.config.ts
@@ -22,12 +22,12 @@ export default defineConfig(({ mode }) => {
           display: 'standalone',
           icons: [
             {
-              src: 'icons/icon-192x192.png',
+              src: 'icons/split-192.png',
               sizes: '192x192',
               type: 'image/png'
             },
             {
-              src: 'icons/icon-512x512.png',
+              src: 'icons/split-512.png',
               sizes: '512x512',
               type: 'image/png'
             }
